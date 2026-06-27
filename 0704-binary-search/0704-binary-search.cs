1838127627e151a70f10b59ce6e8c1bb7175d6af
SLEEPY-sama <walunjrohit38@gmail.com>
2026-06-27 22:41:17 +0530

    Time: 0 ms (100%), Space: 53 MB (61.82%) - LeetHub

--- 0704-binary-search/0704-binary-search.cs
+++ 0704-binary-search/0704-binary-search.cs
@@ -1,36 +1,28 @@
-public class Solution {
-    public int Search(int[] nums, int target) {
-       
-       int right = nums.Length-1;
-       int left = 0;
+public class Solution
+{
+    public int Search(int[] nums, int target)
+    {
+        int left = 0;
+        int right = nums.Length - 1;
 
-       while  (right >= left ){
-        
-        int mid = left + (right- left )/2;
+        while (left <= right)
+        {
+            int mid = left + (right - left) / 2;
 
-        if (nums[mid] < target ){
-            left = mid+1  ;
+            if (nums[mid] == target)
+            {
+                return mid;
+            }
+            else if (nums[mid] < target)
+            {
+                left = mid + 1;
+            }
+            else
+            {
+                right = mid - 1;
+            }
         }
-        else if (nums[mid] > target){
-            right = mid-1 ;
-        }
 
-
-        if (nums[mid]== target){
-            return mid;
-            
-        }
-        
-   
-
-
-       }
-       return -1;
-
-       
-
-
-    
-
+        return -1;
     }
 }
