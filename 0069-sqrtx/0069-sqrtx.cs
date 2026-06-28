2c43f1f7841f5bb95674910074d7ee3ab6d42d12
SLEEPY-sama <walunjrohit38@gmail.com>
2026-06-28 20:57:29 +0530

    Time: 0 ms (100%), Space: 28.9 MB (75.51%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.cs
@@ -0,0 +1,27 @@
+public class Solution {
+    public int MySqrt(int x) {
+       int left = 1;
+       int right = x;
+
+       while (left <= right){
+        
+        int mid = left + (right - left)/ 2;
+        long square = (long)mid*mid;
+
+        if (square == x ){
+            return mid ;
+        }
+        else if (square < x ){
+           left = mid +1;
+        }
+        else if (square > x ){
+            right = mid -1;
+        }
+
+
+
+       } 
+      return right; 
+
+    }
+}
