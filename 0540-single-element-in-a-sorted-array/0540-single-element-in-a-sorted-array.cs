a5f38e8d28f9bd639f997a4b9556cc57a9f671e3
SLEEPY-sama <walunjrohit38@gmail.com>
2026-07-07 21:16:54 +0530

    Time: 0 ms (100%), Space: 50.6 MB (15.43%) - LeetHub

---
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cs
@@ -0,0 +1,23 @@
+public class Solution {
+    public int SingleNonDuplicate(int[] nums) {
+        
+        int left = 0;
+        int right = nums.Length-1;
+
+        while  (left < right ){
+             
+             int mid = left + (right - left )/2;
+
+             if ((mid % 2) == 0 && nums[mid]== nums[mid +1]){
+                left = mid + 2 ;
+             }
+             else if ((mid % 2) != 0 && nums[mid] == nums[mid -1 ]){
+                left = mid + 1;
+             }
+             else {
+                right= mid  ;
+             }
+        }
+        return nums[left];
+    }
+}
